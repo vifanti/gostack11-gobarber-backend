5014c320c234607f74adcb578793f8d9b9b8d833
Vinicius Fantinatto <vifanti@gmail.com>
2020-05-02 19:40:09 -0300

    Configuração de cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -10,6 +11,8 @@ import AppError from './errors/AppError';
 import './database';
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
@@ -22,6 +25,7 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     });
   }
 
+  // eslint-disable-next-line no-console
   console.error(err);
 
   return response.status(500).json({
